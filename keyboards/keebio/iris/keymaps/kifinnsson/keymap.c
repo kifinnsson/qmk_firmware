5f17611a6def6db09a76125ad9a05ce3cedca614
Ki Finnsson <kifinnsson@gmail.com>
2020-03-31 21:25:58 -0600

    keymap.c added ARSTNEIO legend

--- keyboards/keebio/iris/keymaps/kifinnsson/keymap.c
+++ keyboards/keebio/iris/keymaps/kifinnsson/keymap.c
@@ -115,7 +115,63 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   return true;
 }
 
-//combos for arstneio chorded layout
+// ***** Chorded ARSTNEIO Master List *****
+// 3/31/2020: Implimented alpha layer
+// TODO: Number Layer
+// TODO: Reformat so chorded character comes first
+// TODO: Move into QMK userspace
+//
+// Chorded ARSTNEIO Alphas
+//
+// A (HOME)
+// B (T+N)
+// C (R+T)
+// D (S+T)
+// E (HOME)
+// F (R+S)
+// G (T+I)
+// H (N+E)
+// I (HOME)
+// J (A+N)
+// K (R+N)
+// L (N+O)
+// M (S+N)
+// N (HOME)
+// O (HOME)
+// P (A+T)
+// Q (A+E)
+// R (HOME)
+// S (HOME)
+// T (HOME)
+// U (E+I)
+// V (T+E)
+// W (A+R)
+// X (A+S)
+// Y (N+I)
+// Z (R+E)
+//
+// Chorded ARSTNEIO Characters
+//
+// Backspace (A+O)
+// ; (I+O)
+// , (S+E)
+// . (R+I)
+// / (T+O+Shift)
+// ? (T+O)
+// ' (S+O)
+// ! (E+O)
+// @ (E+O+Shift)
+// - (S+I)
+// ( (A+I)
+// ) (R+O)
+// [ (A+I+Shift)
+// ] (R+O+Shift)
+//
+// Chorded ARSTNEIO Whitespace
+//
+// <TAB> (A+R+S+T)
+// <ENTER> (N+E+I+O)
+// <SPACE> (A+R+S+T+N+E+I+O)
 enum combos {
   //
   // Chorded ARSTNEIO Alphas
